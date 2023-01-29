b8ee8bf6ababc40a05185945f71fc586ec48ee78
Luka Brkovic <lucabrkovic@gmx.de>
2023-01-29 15:47:16 +0100

    reg route, adding routes to index. fix getToken

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import dotenv from "dotenv"
 import connectToDatabase from "./database.js"
 import express from "express"
+
+
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+
 const app = express()
 
 dotenv.config()
@@ -11,8 +15,11 @@ app.use(express.json())
 
 const port = process.env.PORT || 5000
 
-
+//! ROUTES 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
+
+
 
 app.listen(port, () => {
     console.log(`Server runs on the port ${port}`)
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,11 +4,11 @@ import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken"
 import { genSalt } from "bcryptjs";
 
-const router = express.Router()
+const userRoutes = express.Router()
 
 //TODO expireIn 60day is just bsc of production (1,2h is normall)
 // id for a user token
-const getToken = (id) => {
+const genToken = (id) => {
     return jwt.sign({id}, process.env.TOKEN_SECRET, {expireIn:"60d"})
 }
 // login user 
@@ -23,7 +23,7 @@ const loginUser = asyncHandler(async(req,res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: genSalt(user._id)
+            token: genToken(user._id)
         })
     } else {
         // * bcs of the async handler this go directly to the frontend
@@ -31,3 +31,39 @@ const loginUser = asyncHandler(async(req,res) => {
         throw new Error("Invalid email or pw")
     }
 })
+
+// register user 
+const registerUser = asyncHandler(async(req,res) => {
+    const {name,email,password} = req.body
+
+    const userExists = await User.findOne({email})
+    if(userExists) {
+        res.status(400)
+        throw new Error ("We are already have an account with that email address.")
+    }
+
+    const user = await User.create({
+        email, 
+        name,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: genToken(user._id)
+        })
+    } else {
+        res.json(400) 
+        throw new Error("Invalid user data")
+    }
+})
+
+// ! ============== ROUTES ===================
+userRoutes.post("/login", loginUser)
+userRoutes.post("/register", registerUser)
+
+export default userRoutes
